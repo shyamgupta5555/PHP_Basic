b67c42433233daba8005af3ef2032e8a86b2e586
shyam <shyamgupta0214@gmail.com>
2023-08-08 00:20:41 +0530

    added

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$a ="learn php";
+//  there are method print variable
+// bo not miss semicolon 
+echo $a;
+print $a;
+
+// array value 
+$cars = array("Volvo", "BMW", "Toyota");
+echo $cars[0];
+
+
+// print 0 to 9 number
+for($a=0;$a<10 ;$a++){
+  echo $a;
+};
+
+
+// call function in php
+
+function php(){
+ echo "hello function";
+};
+
+php();
