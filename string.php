cb92e186c73e752aa4ceca1ff58494c542a76456
shyam <shyamgupta0214@gmail.com>
2023-08-08 00:32:11 +0530

    added

---
+++ string.php
@@ -0,0 +1,16 @@
+<?php
+$a = "hii i am hello word";
+// find length this string
+echo strlen($a);
+
+// find count of word this string
+
+echo str_word_count("convert string".$a."</br>");
+
+// convert reverse string
+
+echo strrev("reverse string".$a);
+
+// replace string
+
+echo str_replace("world", "Dolly", "Hello world!");
